edab6e5579c1f2fbe667f2981aa9b865a2d8a48f
Efe Eroğlu <210541019@firat.edu.tr>
2025-05-18 19:59:26 +0300

    Updates

--- src/data/cta.ts
+++
@@ -1,7 +0,0 @@
-export const ctaDetails = {
-    heading: 'Yapay Zekâ Destekli Öğrenmeye Sizde Katılın',
-    subheading: 'Öğrenio’yu indirerek yapay zekâ destekli aktiviteler ve gerçek zamanlı performans takibiyle eğitim yolculuğunuza güç katın!',
-    appStoreUrl: '#',
-    googlePlayUrl: '#'
-  };
-  
--- src/data/hero.ts
+++
@@ -1,5 +0,0 @@
-export const heroDetails = {
-    heading: 'Akıllı, Güvenli, Basit Öğrenme',
-    subheading: 'Etkileşimli yapay zekâ destekli aktivitelerden gerçek zamanlı performans verilerine kadar, Öğrenio öğretmenlere ve öğrencilerine eğitim yolculuğunda kontrolü ele alma gücü sunar.',
-    centerImageSrc: '/images/8.png',
-}
